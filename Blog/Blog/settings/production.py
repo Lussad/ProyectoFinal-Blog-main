5cbf6b96ed25dc57ec788f58cb6d1dc20e69a716
Lussad <yaelucas00@gmail.com>
2025-08-18 20:09:29 -0300

    Producción

--- Blog/Blog/settings/production.py
+++ Blog/Blog/settings/production.py
@@ -1,5 +1,25 @@
-from .base import *
+from .base import * 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR_STR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR_STR, 'media')
 
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+SECRET_KEY = ''
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carrito$carrito',  # formato: usuario$nombre_basedatos
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'carrito.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
+}
